72662e4e0d9154505b3ceedb108503e9171eeacb
nhantrung <32142863+nhantrung@users.noreply.github.com>
2020-09-11 13:14:23 +0700

    EntitiData

---
+++ ShopSolution.Data/EntitiFranework/ShopBankDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ShopBankSolution.Data.EntitiFranework
+{
+    public class ShopBankDbContextFactory : IDesignTimeDbContextFactory<ShopBankDBContext>
+    {
+        public ShopBankDBContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("ShopBankSolutionDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ShopBankDBContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ShopBankDBContext(optionsBuilder.Options);
+        }
+    }
+}
